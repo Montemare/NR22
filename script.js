3255c5504bb71067734823b03f7cbc56d89bfe06
Montemare <96527159+Montemare@users.noreply.github.com>
2022-09-12 15:02:29 +0200

    NRD22

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const mainMenu = document.querySelector('.main-menu');
+
+hamburger.addEventListener('click', () => {
+    mainMenu.classList.toggle('show');
+
+});
